813cf79c77b87d04f1dcbd5d196fab78c24c4e30
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-11-16 01:57:08 +0100

    lil whoops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import FrontPageVM from './api/pageVMs/FrontPageVM'
+import FrontPageVM from './pageVMs/FrontPageVM'
 
 export default function Home() {
   return (
