579d47297e6399b4a1d7f8081326374d524ea593
SolarEarth37 <92442195+SolarEarth37@users.noreply.github.com>
2022-12-07 11:37:04 +0100

    Made nodemailer work in Typescript

--- src/emailSender/index.tsx
+++ src/emailSender/index.tsx
@@ -1,35 +1,27 @@
-export default function fakenews() {
+import nodemailer from 'nodemailer'
 
+export async function mailSender() {
+    var transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",     // hostname
+        requireTLS: true,               
+        secure: false,                  // TLS requires secureConnection to be false
+        port: 587,                      // port for secure SMTP
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        auth: {
+            user: process.env.MAIL_ADDRESS,
+            pass: process.env.MAIL_PASSWORD,
+        },
+        logger: true
+    })
+
+    let user = "test"                     // This is where I want to pass the user name from the Loan.tsx file
+
+    const info = await transporter.sendMail({
+        from: process.env.MAIL_ADDRESS,
+        to: process.env.ADMIN_MAIL,
+        subject: "Overdue Loan Notification",
+        text: "User " + {user} + "has an overdue loan"
+    });
 }
-
-/*import nodemailer from "nodemailer"
-
-var transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",     // hostname
-    secureConnection: false,        // TLS requires secureConnection to be false
-    port: 587,                      // port for secure SMTP
-    tls: {
-        ciphers: 'SSLv3'
-    },
-    auth: {
-        user: process.env.MAIL_ADDRESS,
-        pass: process.env.MAIL_PASSWORD
-    }
-})
-
-let details = {
-    from: process.env.MAIL_ADDRESS,
-    to: process.env.ADMIN_MAIL,
-    subject: "Overdue Loan Notification",
-    text: "User " + "has an overdue loan"
-}
-transporter.sendMail(details, (err: any) => {
-    if (err) {
-        console.log("error", err)
-    }
-    else {
-        console.log("email has sent!!")
-    }
-})
-
-*/
