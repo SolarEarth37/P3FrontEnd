97be45dcf5a9eac36d1a255dc413477a27b95dae
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-12-07 22:01:07 +0100

    Setting up get and post

--- src/Hooks/useCreateLoan.tsx
+++ src/Hooks/useCreateLoan.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
 
+interface LoanData {
+    User: string,
+    devices?: number[],
+    packages?: number[]
+}
+
 export default function useCreateLoan() {
 
-    return
+    async function sendData(data = {}, url = '/create') {
+
+        const response = await fetch(`${process.env.REACT_APP_API}${url}`, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+
+            body: JSON.stringify(data)
+        })
+        if (response.status === 200) {
+            return await response.json()
+        }
+    }
+    sendData()
 }
--- src/Hooks/useGetLoans.tsx
+++ src/Hooks/useGetLoans.tsx
@@ -2,5 +2,18 @@ import React from 'react'
 
 export default function useGetLoans() {
 
-    return
+    async function sendData(url = '/getLoan') {
+        await fetch(`${process.env.REACT_APP_API}${url}`, {
+            method: 'POST'
+        }).then(async (result) => {
+            if (!result.ok) {
+                throw new Error('Could not connect to server')
+            }
+
+            let res = await result.json()
+
+            return res
+        })
+    }
+    sendData()
 }
