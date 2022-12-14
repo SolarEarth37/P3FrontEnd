c7e2288d7471bc83d812911887a97fd2a3f2a7fa
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-12-14 17:32:44 +0100

    Added warning suppresion in device list

--- src/Base/DeviceList.tsx
+++ src/Base/DeviceList.tsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import getUsers from '../Hooks/useGetUsers'
-import Device, {DeviceData} from '../Composite/Device'
+import Device, { DeviceData } from '../Composite/Device'
 import useGetDevices from '../Hooks/useGetDevices'
 
 export default function UserList() {
   const [allDevices, setAllDevices] = useState<DeviceData[]>([])
-  useEffect(()=>{
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     useGetDevices().then(res => {
       console.log(res)
       setAllDevices(res)
     })
-  },[])
-    
+  }, [])
+
   return (
     <>
-        {allDevices.map((element, i) => (
-            <Device key={i} Name={element?.Name} Id={element.Id} />
-        ))}
+      {allDevices.map((element, i) => (
+        <Device key={i} Name={element?.Name} Id={element.Id} />
+      ))}
     </>
   )
 }
